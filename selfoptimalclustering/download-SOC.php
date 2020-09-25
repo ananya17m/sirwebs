8763e3335b0f14aa41143360699f86eb86abf6be
ananya17m <ananya.am62@gmail.com>
2020-09-25 14:02:45 +0530

    website

---
+++ selfoptimalclustering/download-SOC.php
@@ -0,0 +1,9 @@
+<?php
+
+//$hit_count = @file_get_contents('count-nix.txt');
+//$hit_count++;
+//@file_put_contents('count-nix.txt', $hit_count);
+
+header('Location: SOC.zip'); // redirect to the real file to be downloaded
+
+?>
